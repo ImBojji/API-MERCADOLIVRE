d7f0e88c5cdbf63d26b92e629caad00a747982d1
ImBojji <matheusribeirovogado12@gmail.com>
2025-03-14 17:25:48 -0300

    Tentativa de integrar backend com frondend

--- myproject/api/server.js
+++ myproject/api/server.js
@@ -1,9 +1,31 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.get("/", (req, res) => {
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.get("/api", (req, res) => {
   res.json({ message: "API funcionando!" });
 });
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
+if (process.env.NODE_ENV !== "vercel") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
+}
 
 export default app;
