e56134acee0edfcdeec3794ae24579103edfe3cd
ImBojji <matheusribeirovogado12@gmail.com>
2025-03-17 17:38:28 -0300

    tentativa 2

--- myproject/api/server.js
+++ myproject/api/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import path from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "url"; 
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,22 +10,14 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-
 app.get("/api", (req, res) => {
   res.json({ message: "API funcionando!" });
 });
-
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-
-if (process.env.NODE_ENV !== "vercel") {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-  });
-}
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 export default app;
